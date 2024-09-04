4984746d0cdbb2ef796864fd94ee0cad93c67a9a
mayur-tarlekar3 <mayurtarlekar35@gmail.com>
2024-09-04 14:18:06 -0400

    star loop

--- first.java
+++ first.java
@@ -8,7 +8,7 @@ public class first
         {
             for(int j=1; j<=i;j++)
             {
-                System.out.print(" **");
+                System.out.print(" *");
             }
              System.out.println();
         }
